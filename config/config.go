d116e5b56cb2f1fdc2b268294045db11f9bf6475
Zhenyu Shi <zhenyu.shi@compass.com>
2022-10-21 19:44:16 +0800

    Update config

--- config/config.go
+++ config/config.go
@@ -1,7 +1,11 @@
 package config
 
 import (
+	"fmt"
 	"time"
+
+	"github.com/fsnotify/fsnotify"
+	"github.com/spf13/viper"
 )
 
 var Cfg = &Config{}
@@ -76,5 +80,27 @@ type Prome struct {
 
 // 加载配置，失败直接panic
 func LoadConfig() {
+	viper := viper.New()
 
+	//1.设置配置文件路径
+	viper.SetConfigFile("config/config.yml")
+
+	//2.配置读取
+	if err := viper.ReadInConfig(); err != nil {
+		panic(err)
+	}
+
+	//3.将配置映射成结构体
+	if err := viper.Unmarshal(Cfg); err != nil {
+		panic(err)
+	}
+
+	//4. 监听配置文件变动,重新解析配置
+	viper.WatchConfig()
+	viper.OnConfigChange(func(e fsnotify.Event) {
+		fmt.Println(e.Name)
+		if err := viper.Unmarshal(Cfg); err != nil {
+			panic(err)
+		}
+	})
 }
--- main.go
+++ main.go
@@ -1,3 +1,14 @@
 package main
 
-func main() {}
+import (
+	"fmt"
+	"shop-search-api/config"
+)
+
+func init() {
+	config.LoadConfig()
+}
+
+func main() {
+	fmt.Println(config.Cfg)
+}
